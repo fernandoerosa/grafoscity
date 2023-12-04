eb3ff8b0ae2a3ca90449795eae80820e00abd7b2
iroussenq <igorroussenq273@outlook.com>
2023-12-04 20:43:10 -0300

    inicio do algoritimo do diijsktra

--- av3/src/main/kotlin/com/av3/av3/model/CidadeModel.kt
+++ av3/src/main/kotlin/com/av3/av3/model/CidadeModel.kt
@@ -1,5 +1,6 @@
 package com.av3.av3.model
 
+import lombok.Builder
 import lombok.Getter
 import lombok.RequiredArgsConstructor
 import lombok.Setter
@@ -12,10 +13,10 @@ import java.util.LinkedList
 class CidadeModel: Comparable<CidadeModel>{
 
     //Entender melhor lateinit
-    private final lateinit var nome: String;
-    private var distancia: Int = Integer.MAX_VALUE;
-    private var maisProximo: MutableList<CidadeModel> = LinkedList();
-    private var cidadesAdjacente: MutableMap<CidadeModel, Int> = HashMap();
+    public final lateinit var nome: String;
+    public var distancia: Int = Integer.MAX_VALUE;
+    public var maisProximo: MutableList<CidadeModel> = LinkedList();
+    public var cidadesAdjacente: MutableMap<CidadeModel, Int> = HashMap();
 
     fun adicionarCidadeAdjacente(cidade: CidadeModel, peso: Int) {
         cidadesAdjacente.put(cidade, peso);
--- av3/src/main/kotlin/com/av3/av3/service/TrackerService.kt
+++ av3/src/main/kotlin/com/av3/av3/service/TrackerService.kt
@@ -1,8 +1,10 @@
 package com.av3.av3.service
 
+import com.av3.av3.model.CidadeModel
 import lombok.NoArgsConstructor
 
 @NoArgsConstructor
 interface TrackerService {
-    fun calcularDistancia(): String;
+    fun calcularDistanciaMaisCurta(cidade: CidadeModel);
+
 }
